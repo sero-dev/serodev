3d52e915282b6352b5efd54fe34ba3c8201f94f1
Sean Rodriguez <sean.rodriguez@outlook.com>
2024-02-09 16:32:28 -0500

    Add web-app application

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'serodev-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    selector: 'serodev-root',
+    template: `
+    <div class="flex">
+      <serodev-navigation-bar />
+      <div class="grow flex flex-col bg-stone-200">
+        <div class="flex justify-end bg-blue-700 min-h-10">
+        </div>
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+
+  `,
+    styles: ``,
+    imports: [RouterModule, NavigationBarComponent]
 })
 export class AppComponent {
-  title = 'web-app';
 }
---
+++ apps/web-app/src/app/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'serodev-navigation-bar',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="max-h-screen min-h-screen w-64 bg-white">
+
+    </div>
+  `,
+  styles: ``,
+})
+export class NavigationBarComponent {}
